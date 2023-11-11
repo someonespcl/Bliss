b5e3bd53f3cce32717c69004ff15214390ce8230
someonespcl <madhavpoola4@gmail.com>
2023-11-12 00:15:37 +0530

    added register screen

---
+++ app/src/main/java/com/bliss/activities/RegisterActivity.java
@@ -0,0 +1,11 @@
+package com.bliss.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
